95a5033efa6653b02881af51380535e866a6a92a
iezerbaev <i.ezerbaev97@gmail.com>
2021-07-28 13:04:40 +0300

    add recursive_len find list

--- recursion.py
+++ recursion.py
@@ -1,7 +1,13 @@
 # recursion factorial
-def fact(x): 
+def fact(x):
     if x == 1:
         return 1
-    return x * fact(x - 1) 
+    return x * fact(x - 1)
 
 
+
+#recursive len list
+def recursive_len(some_list):
+    if some_list == []:
+        return 0
+    return 1 + recursive_len(some_list[1:])
