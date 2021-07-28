ed8b2efd081aa1b56efb234d5fea1179e71fc802
iezerbaev <i.ezerbaev97@gmail.com>
2021-07-28 13:26:19 +0300

    add some recursive algorithms

--- recursion.py
+++ recursion.py
@@ -5,9 +5,24 @@ def fact(x):
     return x * fact(x - 1)
 
 
-
-#recursive len list
+# recursive len list
 def recursive_len(some_list):
     if some_list == []:
         return 0
     return 1 + recursive_len(some_list[1:])
+
+
+# recursive tribonacci
+def tribonacci(n):
+    if n in (0, 1):
+        return 0
+    elif n == 2:
+        return 1
+    return tribonacci(n - 1) + tribonacci(n - 2) + tribonacci(n - 3)
+
+
+# recursive list sum
+def rec_linear_sum(some_list):
+    if some_list == []:
+        return 0
+    return some_list[0] + rec_linear_sum(some_list[1:])
